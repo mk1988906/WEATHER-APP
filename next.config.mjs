ce3ec2f3ea3ddcb16002c15c73804650b7ca6579
vermamanish956 <vermamanish956@gmail.com>
2024-08-14 20:18:22 +0530

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+export const images = {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+    ],
+  };
+  
